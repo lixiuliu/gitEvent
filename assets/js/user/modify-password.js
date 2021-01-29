ac1681e21373d02be63117ce11977b5a243cc864
lixiu <1262536498@qq.com>
2021-01-29 19:41:37 +0800

    user部分书写

---
+++ assets/js/user/modify-password.js
@@ -0,0 +1,29 @@
+// 修改密码模块
+$(function() {
+    // 1.表单校验
+    const { form, layer } = layui
+    // 1.表单校验
+    form.verify({
+            pass: [
+                /^\w{6,12}$/,
+                '密码必须6到12位，且不能出现空格'
+            ],
+            confirmPass: function(val) {
+                if (val !== $('#pass').val()) {
+                    return '两次密码输入不一致'
+                }
+            }
+        })
+        // 表单提交
+    $('.layui-form').submit(function(e) {
+        e.preventDefault()
+        axios.post('/my/updatepwd', $(this).serialize())
+            .then(res => {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('修改密码失败')
+                }
+                layer.msg('修改密码成功')
+            })
+    })
+})
