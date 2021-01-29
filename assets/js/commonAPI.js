19275208f8bed9eb47da64d3b783d7f1d8236c2d
lixiu <1262536498@qq.com>
2021-01-29 15:25:12 +0800

    完成了首页

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -1,7 +1,19 @@
 // 为全局的axios请求设置路径
-axios.defaults.baseURL = 'http://ajax.frontend.itheima.net'
+axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net'
     // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
+    console.log('-----发送ajax请求前', config);
+    console.log(config.url);
+    // 获取本地存储token令牌
+    const token = localStorage.getItem('token') || ''
+        // 在发送请求之前判断是否有/my开头的请求路径
+        // 如果有，手动添加header请求头
+        // 1. startsWith
+        // 2.正则表达式
+        // 3.indexOf('/my')==0
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
     // 在发送请求之前做些什么
     return config;
 }, function(error) {
@@ -12,6 +24,12 @@ axios.interceptors.request.use(function(config) {
 
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
+    console.log('---发送ajax响应前', response);
+    const { message, status } = response.data
+    if (message == '身份认证失败！' && status == 1) {
+        localStorage.removeItem('token')
+        location.href = './login.html'
+    }
     // 对响应数据做点什么
     return response.data;
 }, function(error) {
---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+$(function() {
+    // 从layui中提取模块
+    const { layer } = layui
+    // 1.获取用户的个人信息
+    function getUserInfo() {
+        // 发送ajax请求
+        axios.get('/my/userinfo').then(res => {
+            console.log(res);
+            // 校验请求失败
+            if (res.status !== 0) {
+                return layer.msg('获取用户信息失败')
+            }
+            const { data } = res
+            // 渲染用户信息
+            const name = data.nickname || data.username
+            $('.nickname').text(`欢迎${name}`).show()
+                // 渲染头像
+            if (data.usre_pic) {
+                $('.avater').prop('src', data.user_pic).show()
+                $('text-avater').hide()
+            } else {
+                $('.text-avater').text(name[0].toUpperCase()).show()
+                $('.avater').hide()
+            }
+        })
+
+    }
+    getUserInfo()
+        // 点击退出
+    $('#logout').click(function() {
+        // 请求接口（模拟）
+        // 清除token 令牌
+        localStorage.removeItem('token')
+        location.href = './login.html'
+    })
+})
